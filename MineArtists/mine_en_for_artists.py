bdb206607feea051627dce4cbbe1b9086a5a929d
tb2332 <tb2332@columbia.edu>
2010-03-28 01:14:35 -0400

    almost ready to run, not debugged

--- MineArtists/mine_en_for_artists.py
+++ MineArtists/mine_en_for_artists.py
@@ -6,9 +6,39 @@ tb2332@columbia.edu
 
 import os
 import sys
+import time
+import numpy as np
+# sqlite stuff
 import sqlite3
 import sqlite3.dbapi2 as sqlite
+# echonest stuff
+from pyechonest import config
+from pyechonest import artist as artistEN
+try:
+    config.ECHO_NEST_API_KEY = os.environ['ECHO_NEST_API_KEY']
+except:
+    config.ECHO_NEST_API_KEY = os.environ['ECHONEST_API_KEY']
 
+
+
+
+
+def table_exists(cursor,tablename):
+    """
+    Checks if a table exists.
+    Trick taken from:
+    http://sqlserver2000.databases.aspfaq.com/
+    how-do-i-determine-if-a-table-exists-in-a-sql-server-database.html
+    """
+    query =  'IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES ' 
+    query += "WHERE TABLE_TYPE='BASE TABLE' "
+    query += "AND TABLE_NAME='" + tablename + "') " 
+    query += "SELECT 1 ELSE SELECT 0" 
+    cursor.execute(query)
+    res = cursor.fetchone()[0]
+    if res == 1:
+        return True
+    return False
 
 def mine(sqlite_db,maxartists=1000000):
     """
@@ -17,18 +47,69 @@ def mine(sqlite_db,maxartists=1000000):
     mine similar artists.
     """
 
+    # start time
+    tstart = time.time()
+
     # connects to the DB, creates it if necessary
     connection = sqlite.connect(sqlite_db)
+    # gets cursor
+    cursor = connection.cursor()
+
+    # db empty? create table, add Beatles
+    #cursor.execute('CREATE TABLE artists (id INTEGER PRIMARY KEY,name VARCHAR(50), checked INTEGER)')
+    #cursor.execute('INSERT INTO names VALUES (null, "The Beatles",0)')
 
 
+    try:
+        while True:
+            # check if too many artists
+            cursor.execute('SELECT COUNT(id) FROM artists')
+            nArtists = cursor.fetchone()[0]
+            if nArtist > maxartists:
+                break
+            
+            # get an artist not checked
+            cursor.execute('SELECT name FROM names WHERE checked=0')
+            unchecked = cursor.fetchmany(1000)
+            unchecked_artist = unchecked[np.random.randint(len(unchecked))][0]
+
+            # find similar artists
+            aEN = artistEN.search_artists(unchecked_artist)[0]
+            asim = aEN.similar(rows=100)
+            
+            # add them to the database
+            for a in asim:
+                # already in?
+                query = 'SELECT name FROM artists WHERE name='
+                query += a.name
+                cursor.execute(query)
+                found = cursor.fetchmany(2)
+                if len(found) == 0:
+                    query = 'INSERT INTO artists VALUES (null, "'
+                    query += a.name + '",0)'
+                    cursor.execute(query)
+
+            # check in the query artist
+            query = 'UPDATE artists SET checked=1 WHERE name='
+            query += unchecked_artist
+            cursor.execute(query)
+
+            # commit
+            connection.commit()
 
 
+            raise NotImplementedError
+    except:
+        print "ERROR:", sys.exc_info()[0]
+        connection.commit()
+        connection.close()
+        return
 
-
-    raise NotImplementedError
-
-
-
+    # finish correctly
+    connection.commit()
+    connection.close()
+    print 'EchoNest artist name mining finished correctly'
+    print 'program ran for',time.time()-tstart,'seconds'
 
 
 
