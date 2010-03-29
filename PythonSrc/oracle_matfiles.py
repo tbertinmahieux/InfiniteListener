7f9baad907c8ca8519107cf64bdac1bec54a673b
tb2332 <tb2332@columbia.edu>
2010-03-29 16:44:03 -0400

    to get data from matfiles, similar to BostonHackDay project

---
+++ PythonSrc/oracle_matfiles.py
@@ -0,0 +1,15 @@
+"""
+Oracle to go get data from Matfiles.
+Usefull to reproduces experiments from ISMIR 2010 paper
+or initialize a codebook wth some downloaded data.
+
+T. Bertin-Mahieux (2010) Columbia University
+tb2332@columbia.edu
+"""
+
+
+import os
+import sys
+import time
+import copy
+import numpy as np
