ee5351856c9f2d1d86ce322ceb0d404edbe51b9a
tb2332 <tb2332@columbia.edu>
2010-03-29 17:05:07 -0400

    moving feature creation into the oracle, and passing a param dictionary at creation

--- PythonSrc/oracle_matfiles.py
+++ PythonSrc/oracle_matfiles.py
@@ -13,3 +13,81 @@ import sys
 import time
 import copy
 import numpy as np
+# features stuff
+import features
+
+
+# ORACLE CLASS
+
+class OracleMatfiles():
+    """
+    Class to get EchoNest features
+    """
+
+    def __init__(self,params,folder):
+        """
+        Constructor, get a folder name containing matfiles in any subdirectory.
+        """
+        # features stuff
+        self._pSize = params['pSize']
+        self._usebars = params['usebars']
+        self._keyInv = params['keyInv']
+        self._songKeyInv = params['songKeyInv']
+        self._positive = params['positive']
+        self._do_resample = params['do_resample']
+        # find all matfiles
+        self._matfiles = get_all_matfiles(folder)
+        assert len(self._matfiles) > 0,'no matfiles found in %s'%folder
+        # statistics
+        self._nTracksGiven = 0
+
+
+    def next_track(self):
+        """
+        Returns features for a random matlab file, or None if some error.
+        """
+        matfile = _matfiles[np.random.randint(len(self._matfiles))]
+        return features.features_from_matfile(matfile,
+                                              pSize=self._pSize,
+                                              usebars=self._usebars,
+                                              keyInv=self._keyInv,
+                                              songKeyInv=self._songKeyInv,
+                                              positive=self._positive,
+                                              do_resample=self._do_resample)
+
+    def tracksGiven(self):
+        """
+        Return the number of tracks given since the creation of
+        ths instance.
+        """
+        return self._nTracksGiven
+
+
+def get_all_matfiles(basedir) :
+    """
+    From a root directory, go through all subdirectories
+    and find all matlab files. Return them in a list.
+    """
+    allfiles = []
+    for root, dirs, files in os.walk(basedir):
+        matfiles = glob.glob(os.path.join(root,'*.mat'))
+        for f in matfiles :
+            allfiles.append( os.path.abspath(f) )
+    return allfiles
+
+
+def die_with_usage():
+    """
+    HELP MENU
+    """
+    print 'Class and function to serve as a downloaded data Oracle'
+    print 'Data takes the form of matfiles containing beat chromas'
+    print 'Similar to ISMIR 2010 project (Boston HackDay)'
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    # help menu than exit
+    die_with_usage()
+
+        
