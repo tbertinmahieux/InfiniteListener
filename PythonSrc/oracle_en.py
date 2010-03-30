6dfcf5eaf2f655c001e91d1f2c4966ef4168ab2c
tb2332 <tb2332@columbia.edu>
2010-03-30 19:00:57 -0400

    typo

--- PythonSrc/oracle_en.py
+++ PythonSrc/oracle_en.py
@@ -88,7 +88,7 @@ def _thread_en(artistsdb):
         # success rate too low? print WARNING
         cnt_provided += 1
         if cnt_provided % 100 == 0:
-            prob_provide = ,cnt_provided*100./cnt_iter
+            prob_provide = cnt_provided*100./cnt_iter
             if prob_provide < 85.:
                 print 'WARNING: _en_thread, prob. of providing is low:',prob_provide,'% , artists do not actually have song?'
 
