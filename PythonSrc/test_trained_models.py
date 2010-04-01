6540056c98655395f711ae9779b05c7c4d147745
tb2332 <tb2332@columbia.edu>
2010-03-31 22:22:39 -0400

    improving

--- PythonSrc/test_trained_models.py
+++ PythonSrc/test_trained_models.py
@@ -16,7 +16,19 @@ import numpy as np
 
 import model as MODEL
 import oracle_matfiles as ORACLE
+import analyze_saved_model as ANALYZE
 
+
+def print_write(s,fname):
+    """
+    Prints something and write it to file
+    """
+    print s
+    f = open(fname,'a')
+    f.write(s)
+    f.write('\n')
+    f.close()
+    
 
 def die_with_usage():
     """
@@ -53,18 +65,56 @@ if __name__ == '__main__':
         sys.argv.pop(1)
 
     # params
-    savedmodel = sys.argv[1]
-    matfilesdir = sys.argv[2]
-    output = sys.argv[3]
+    savedmodel = os.path.abspath(sys.argv[1])
+    matfilesdir = os.path.abspath(sys.argv[2])
+    output = os.path.abspath(sys.argv[3])
+    print_write('saved model = '+ savedmodel,output)
+    print_write('matfiles dir = '+ matfilesdir,output)
+    print_write('output = '+output,output)
 
+
+    #******************************************************************
     # gather in a set all models to try
     # also gather the number of iterations associated with each model
+    if os.path.isfile(savedmodel):
+        # special case, matfile
+        print_write('Doing one file (codebook).',output)
+        all_to_test = [savedmodel]
+    elif testone:
+        # load one things and move on
+        print_write('Doing only one file.',output)
+        all_to_test = [savedmodel]
+    else:
+        # main algorithm
+        parentdir,tmp = os.path.split(savedmodel)
+        # traceback
+        tb = ANALYZE.traceback(savedmodel)
+        print_write('*** TRACEBACK ****************')
+        for f in tb:
+            print_write(str(f),output)
+        print_write('******************************')
+        # find everything in parent folder, then just folders
+        all_in_folder = glob.glob(os.path.join(parentdir,'*'))
+        all_in_folder = filter(lambda x: os.path.isdir(x), all_in_folder)
+        # keep those that have same origin
+        leaves = filter(lambda x: ANALYZE.traceback(x)==tb[0],all_in_folder)
+        # everything to test, matfile at the end
+        all_to_test = set()
+        for f in tb:
+            if os.path.isdir(f):
+                all_to_test.add(f)
+        for f in leaves:
+            all_to_test.add(f)
+        all_to_test = list(all_to_test)
+        all_to_test.append(tb[0])
+    print_write('number of models to test: '+len(all_to_test),output)
+    
 
-
+    #******************************************************************
     # load data into memory
 
 
+    #******************************************************************
     # predict on every model
 
 
-    # write everything to file
