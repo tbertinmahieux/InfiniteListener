60e35aa11d3cae8352f685242626a11f31ebbe15
tb2332 <tb2332@columbia.edu>
2010-04-12 14:50:46 -0400

    debugging

--- PythonSrc/trim_savedmodels_dir.py
+++ PythonSrc/trim_savedmodels_dir.py
@@ -14,6 +14,7 @@ import os
 import sys
 import glob
 import numpy as np
+import shutil
 
 import analyze_saved_model as ANALYZE
 from trainer import StatLog
@@ -128,5 +129,5 @@ if __name__ == '__main__':
     # if not dry run, remove
     if not dryrun:
         for k in to_delete:
-            os.rmdir(k)
+            shutil.rmtree(k)
 
