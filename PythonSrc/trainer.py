fcde5ee08fdf6cc48eb1c082c41eabe048b3c2a5
tb2332 <tb2332@columbia.edu>
2010-04-14 17:42:03 -0400

    exit code added to training, useful for restarting expeirment on NYU cluster

--- PythonSrc/trainer.py
+++ PythonSrc/trainer.py
@@ -164,6 +164,10 @@ def train(savedmodel, expdir='', pSize=8, usebars=2, keyInv=True,
     except:
         print ''
         exc_type, exc_value, exc_traceback = sys.exc_info()
+        # normal stop or not? normal = StopIteration
+        exit_code = 1
+        if str(exc_type) == "<type 'exceptions.StopIteration'>":
+            exit_code = 0 # normal exit
         print "ERROR:", exc_type
         if str(exc_type) != "<type 'exceptions.KeyboardInterrupt'>":
             print '********** DEBUGGING INFO *******************'
@@ -183,7 +187,7 @@ def train(savedmodel, expdir='', pSize=8, usebars=2, keyInv=True,
                                   crash=True)
         print 'saved to: ',savedir
         #quit
-        return
+        return exit_code
 
 
 
