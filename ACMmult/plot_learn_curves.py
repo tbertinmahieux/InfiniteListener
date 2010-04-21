3bdaa9ca9d32995d83d698e35078c2e73309dbe6
tb2332 <tb2332@columbia.edu>
2010-04-21 16:42:01 -0400

    debugging

--- ACMmult/plot_learn_curves.py
+++ ACMmult/plot_learn_curves.py
@@ -41,7 +41,8 @@ def plot_from_file(filename):
     curve = []
     # iterate over lines
     while True:
-        line = f.next().strip()
+        line = f.next()
+        print 'line=',line
         if line[:len('#PLOTEND')] == '#PLOTEND':
             if len(curve) > 0:
                 curves.append( curve )
