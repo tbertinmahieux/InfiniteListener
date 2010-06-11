f9575aa83c8bdc2b2116e6b3b163d7a9cd551635
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-06-11 15:48:23 -0400

    added So and Su

--- PythonSrc/Segmentation/measures.py
+++ PythonSrc/Segmentation/measures.py
@@ -114,3 +114,70 @@ def count_similar_pairs(cuts,assume0=True):
     # done, return
     return total
     
+
+
+def entropy_So_Su(startbref,stopbref,startbcand,stopbcand):
+    """
+    Computes entropy measures based on Lukashevich 2008
+    First, H(estimated | ground truth) than H(gound truth | estimated)
+    """
+    assert stopbref[-1] == stopbcand[-1], "different end beat for cand and ref?"
+    assert len(np.unique(startbref)) == len(startbref), 'empty segs in reference'
+    assert len(np.unique(startbcand)) == len(startbcand), 'empty segs in cadidate'
+    assert startbref[0] == 0, 'ref start beat not 0?'
+    assert startbcand[0] == 0, 'cand start beat not 0?'
+    # get last beat
+    lastb = stopbref[-1]
+    # compute number of segments (a -> ground truth, e -> candidate)
+    Na = len(startbref)
+    Ne = len(startbcand)
+    # get belonging of eahc beat to a segment in both ref and cand
+    beat_per_a_seg = np.zeros(lastb+1)
+    for k in range(len(startbref)):
+        beat_per_a_seg[startbref[k]:stopbref[k]+1] = k
+    beat_per_e_seg = np.zeros(lastb+1)
+    for k in range(len(startbcand)):
+        beat_per_e_seg[startbcand[k]:stopbcand[k]+1] = k
+    # computes n_ij, number of frames that belong simultaneously to segment i of ref
+    # and j of candidate
+    Nij = np.zeros([Na,Ne])
+    for k in range(lastb+1):
+        Nij[ beat_per_a_seg[k], beat_per_e_seg[k] ] += 1
+    # compute H(e|a)
+    Hea = 0
+    for sega in range(len(startbref)):
+        nia = np.sum(Nij[sega,:])
+        pia = nia / (lastb+1.)
+        for sege in range(len(startbcand)):
+            pjiea = Nij[sega,sege] / nia
+            if pjiea == 0:
+                continue
+            Hea -= pia * pjiea * np.log2(pjiea)
+    assert not np.isnan(Hea),'Hea nan'
+    # compute H(a|e)
+    Hae = 0
+    for sege in range(len(startbcand)):
+        nje = np.sum(Nij[:,sege])
+        pje = nje / (lastb+1.)
+        for sega in range(len(startbref)):
+            pijae = Nij[sega,sege] / nje
+            if pijae == 0:
+                continue
+            Hae -= pje * pijae * np.log2(pijae)
+    assert not np.isnan(Hae),'Hae nan'
+    # compute So and Su
+    So = 1 - Hea / np.log2( Ne )
+    Su = 1 - Hae / np.log2( Na )
+    # done, return
+    return So, Su
+
+
+
+def prec_rec_f_So_Su(startbref,stopbref,startbcand,stopbcand):
+    """
+    Convenience function, computes the 5 measures used in R. Weiss' paper
+    at ISMIR 2010
+    """
+    prec, rec, f = pairwise_prec_rec_f(startbref,stopbref,startbcand,stopbcand)
+    So, Su = entropy_So_Su(startbref,stopbref,startbcand,stopbcand)
+    return prec, rec, f, So, Su
