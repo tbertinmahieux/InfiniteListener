d2b7a2d9cce9057b560f9aba3135ac824f489f10
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-06-30 13:32:12 -0400

    version sent to Phil

--- PythonSrc/Segmentation/forphil/extract_landmarks.py
+++ PythonSrc/Segmentation/forphil/extract_landmarks.py
@@ -11,3 +11,35 @@ tb2332@columbia.edu
 import os
 import sys
 import numpy as np
+import audioio
+# wrapper around Dan's fingerprinting code
+# mlab imported there
+from fingerprint import get_landmarks as LANDMARKS
+
+
+def extract(mp3file,npyfile=''):
+    """
+    Get an mp3 file, reads it, extract fingerprints features,
+    saves it to a given npyfile (if different than '')
+    RETURN numpy array for debugging purposes
+    """
+    # read mp3 through MATLAB mp3 file
+    x,fs,tmp = audioio.audioread(mp3file)
+    x = np.average(x,axis=1)
+    assert x.shape[0] > 2,'bad signal averaging'
+    # get the fingerprint features
+    L,S,T,maxes = LANDMARKS.find_landmarks(x,fs)
+    # create the maxes matrix
+    maxes = maxes - 1 # matlab starts at 0
+    nfreq = 256
+    assert np.max(maxes[1,:]) < nfreq,'max freq too high'
+    nsamp = int(np.max(maxes[0,:]) + 1)
+    data = np.zeros([nfreq,nsamp])
+    # fill in data
+    for m in range(maxes.shape[1]):
+        data[maxes[1,m],maxes[0,m]] += 1
+    # save to npy
+    if npyfile != '':
+        np.save(npyfile,data)
+    # return data for debugging or ipython usage
+    return data
