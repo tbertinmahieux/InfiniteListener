6a166168353741cecc3efed802268f47fc5bd733
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-06-30 12:57:34 -0400

    main code

---
+++ PythonSrc/Segmentation/forphil/extract_landmarks.py
@@ -0,0 +1,13 @@
+"""
+Code to easily extract fingerprint features and save them in a
+numpy array file.
+Relies on Dan's code in matlab, and mlabwrap to access it.
+
+Thierry Bertin-Mahieux (2010) Columbia University
+tb2332@columbia.edu
+"""
+
+
+import os
+import sys
+import numpy as np
