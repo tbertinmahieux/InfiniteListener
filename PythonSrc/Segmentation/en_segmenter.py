2008d0e7cdc178493520b94f73bcb6995a60aa4c
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-08-24 12:57:58 -0400

    to test EN features for segmentation

---
+++ PythonSrc/Segmentation/en_segmenter.py
@@ -0,0 +1,9 @@
+"""
+Simple method to perform song segmentation using Ron's method and
+Echo Nest chroma features.
+
+Also, can use codebook encoding.
+
+T. Bertin-Mahieux (2010) Columbia University
+tb2332@columbia.edu
+"""
