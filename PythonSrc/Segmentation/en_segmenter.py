7e034b0f0ebb29205b8a479d320a335a0dcc0c28
Thierry Bertin-Mahieux <tb2332@columbia.edu>
2010-08-24 16:40:40 -0400

    works for all the beatles, no encoding yet

--- PythonSrc/Segmentation/en_segmenter.py
+++ PythonSrc/Segmentation/en_segmenter.py
@@ -26,6 +26,19 @@ _audio_dir = os.path.expanduser('~/Columbia/InfiniteListener/beatles_audio')
 _seglab_dir = os.path.expanduser('~/Columbia/InfiniteListener/beatles_seglab')
 
 
+def get_all_files(basedir,pattern='*.wav') :
+    """
+    From a root directory, go through all subdirectories
+    and find all files that fit the pattern. Return them in a list.
+    """
+    allfiles = []
+    for root, dirs, files in os.walk(basedir):
+        files = glob.glob(os.path.join(root,pattern))
+        for f in files :
+            allfiles.append( os.path.abspath(f) )
+    return allfiles
+
+
 
 def siplca_btchroma(btchroma,rank=4,win=60,plotiter=0,printiter=10,niter=200,alphaZ=0):
     """
@@ -97,11 +110,11 @@ def encode_and_eval_one_beatle_song(wavfile,rank=16,win=40,plotiter=0,printiter=
     # test result
     gtlabels = labfile_to_framewise_label(labfile,bttimes)
     results = evaluate_segmentation(labels,gtlabels,Z)
-    print results
+    return results
 
 
 
-def encode_and_eval_all_beatles_songs(beatlesdir='',rank=16,win=40,plotiter=0,printiter=50,
+def encode_and_eval_all_beatle_songs(beatlesdir='',rank=16,win=40,plotiter=0,printiter=50,
                                       niter=200,alphaZ=-0.005):
     """
     Perform experiment on the whole beatle collection
@@ -109,3 +122,33 @@ def encode_and_eval_all_beatles_songs(beatlesdir='',rank=16,win=40,plotiter=0,pr
     if beatlesdir == '':
         beatlesdir = _audio_dir
     # get all wav files
+    wavfiles = get_all_files(beatlesdir,'*.wav')
+    # encode all files
+    allresults = []
+    for k in range(len(wavfiles)):
+        wavfile = wavfiles[k]
+        if k % 10 == 0 and k > 0:
+            print 'doing',k,'/',len(wavfiles),':',wavfile
+        result = encode_and_eval_one_beatle_song(wavfile,rank=rank,win=win,
+                                                 plotiter=plotiter,printiter=printiter,
+                                                 niter=niter,alphaZ=alphaZ)
+        allresults.append(result)
+        
+    # print results
+    print 'results on',len(wavfiles),'files.'
+    avg_effrank = np.mean( map(lambda x: x['effrank'],allresults) )
+    print 'avg effrank =',avg_effrank
+    avg_nsegments = np.mean( map(lambda x: x['nsegments'],allresults) )
+    print 'avg nsegments =',avg_nsegments
+    avg_nlabels = np.mean( map(lambda x: x['nlabels'],allresults) )
+    print 'avg nlabels =',avg_nlabels
+    avg_pfm = np.mean( map(lambda x: x['pfm'],allresults) )
+    avg_ppr = np.mean( map(lambda x: x['ppr'],allresults) )
+    avg_prr = np.mean( map(lambda x: x['prr'],allresults) )
+    print 'avg pfm =', avg_pfm
+    print 'avg ppr =', avg_ppr
+    print 'avg prr =', avg_prr
+    avg_Su = np.mean( map(lambda x: x['Su'],allresults) )
+    avg_So = np.mean( map(lambda x: x['So'],allresults) )
+    print 'avg Su =', avg_Su
+    print 'avg So =', avg_So
